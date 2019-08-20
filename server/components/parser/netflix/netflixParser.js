c4dc1abb36ac7a354fd9918d623b91a357688d97
jaeyeon kim <foodsksms@gmail.com>
2019-08-21 00:57:08 +0900

    [jy-issue-22] netflix 가입 확인
    - netflix 결제 영수증 이슈로 우선적으로 netflix가입 여부만 확인.
    - netflix의 경우 멤버십을 이용한 사람들만 info@mailer.netflix.com에서 이메일이 온다.
    - commonParser.check(response)에서 netflix도메인이 있으면 return 'netflix'하기때문에 Netflix가입 여부를 확인 할 수 있다.

--- server/components/parser/netflix/netflixParser.js
+++ server/components/parser/netflix/netflixParser.js
@@ -2,14 +2,14 @@ const cheerio = require('cheerio');
 const commonParser = require('../commonParser');
 
 const getNetflixInfo = (response) => {
-  require('fs').writeFile('netflixReceipt.html', commonParser.base64ToUtf8(response), 'utf8');
   const jsonObject = commonParser.stringToJsonObject(commonParser.base64ToUtf8(response));
   const dom = commonParser.convertHtml(commonParser.base64ToUtf8(jsonObject.payload.parts[1].body.data));
-  console.log(dom);
   const $ = cheerio.load(dom);
   service = {};
   service.fromEmail = getFromEmail(response);
   service.email = commonParser.getEmailId(response);
+  service.name = $('body > div > xmeta > xmeta > table.container > tbody > tr > td > table.shell > tbody > tr > td > table > tbody > tr:nth-child(9) > td > table > tbody > tr:nth-child(2) > td').text();
+  console.log('name : ', service.name);
   // service.name = $('span[class=title]').contents().get('0').data;
   // service.price = convertPrice($('body > table > tbody > tr > td > div.aapl-desktop-div > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(3) > td.price-cell > span').text());
   // service.date = convertDateReg($('body > table:nth-child(4) > tbody > tr > td > div.aapl-desktop-div > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td').text())
@@ -23,7 +23,6 @@ const getFromEmail = (response) => {
 }
 
 const fromEmailReg = (fromEmail) => {
-  console.log('delivered email : ', fromEmail);
   const emailReg = /<(.*?)>/;
   return emailReg.exec(fromEmail)[1];
 };
--- server/components/parser/netflix/netflixParserTest.js
+++ server/components/parser/netflix/netflixParserTest.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const {assert} = require('chai');
 const netflixParser = require('./netflixParser');
+const commonParser = require('../commonParser');
 
 describe('# Netflix Parser테스트', () => {
   const response = fs.readFileSync('netflixReceipt.json');
   it('fromEmail가져오기', () => {
     assert.strictEqual(netflixParser.getFromEmail(response), 'info@mailer.netflix.com');
   });
-  it('멤버십 가져오기', () => {
-    assert.strictEqual(netflixParser.getNetflixInfo.plan, '프리미엄');
+  it('netflix 가입여부', () => {
+    assert.strictEqual(commonParser.checkDomain(response), 'netflix');
   });
 })
